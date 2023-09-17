7ed86aee978b13c8076fd7bc21006bcdb25aea11
ziianw <102005289+ziianw@users.noreply.github.com>
2023-09-18 00:05:30 +0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+const dynamicText = document.querySelector("h3 span");
+const words = [" Web Developer", " App Developer", " Networking"];
+
+// Variables to track the position and deletion status of the word
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex);
+
+    // Modifikasi warna teks dan efek pengetikan
+    dynamicText.style.color = "black";
+    dynamicText.style.textDecoration = "none";
+    dynamicText.textContent = currentChar;
+
+    if (!isDeleting && charIndex < currentWord.length) {
+        // If condition is true, type the next character
+        charIndex++;
+        setTimeout(typeEffect, 200);
+    } else if (isDeleting && charIndex > 0) {
+        // If condition is true, remove the previous character
+        charIndex--;
+        setTimeout(typeEffect, 100);
+    } else {
+        // If word is deleted then switch to the next word
+        isDeleting = !isDeleting;
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+        setTimeout(typeEffect, 1200);
+    }
+};
+
+// Memanggil fungsi untuk pertama kali
+typeEffect();
